52ce4a6f5e38ad3167e407c7612966011a89c200
ananyasr29 <ananyasr29jan@gmail.com>
2026-06-10 19:47:07 +0530

    Add files via upload

---
+++ Week 2/Day 3/w2d3q3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n=s.length();
+        for(int len=1;len<=n/2;len++){
+            if(n%len==0){
+                String part=s.substring(0,len);
+                String result="";
+                for(int i=0;i<n/len;i++){
+                    result+=part;
+                }
+                if(result.equals(s))
+            {
+                return true;
+            }
+            }
+        }
+      return false;  
+    }
+}
